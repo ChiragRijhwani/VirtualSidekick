b09737ec17dc6bbcef895f2543e41eb4ec5d5a20
Chirag Rijhwani <chirag.rijhwani@lntinfotech.com>
2020-09-26 01:48:45 +0530

    Added header, footer and login components

--- ui-portal/src/app/app.module.ts
+++ ui-portal/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GroupsComponent } from './groups/groups.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GroupsComponent
+    GroupsComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- ui-portal/src/app/groups/groups.component.ts
+++ ui-portal/src/app/groups/groups.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupsComponent implements OnInit {
 
+  title = "Groups";
+
+  data = [
+    {
+      "name": "Writers",
+      "description": "The content writers",
+      "powers": ["Read", "Write"]
+    },
+    {
+      "name": "Readers",
+      "description": "The content readers",
+      "powers": ["Read"]
+    },
+    {
+      "name": "Publishers",
+      "description": "I approve your content",
+      "powers": ["Read", "Write", "Delete", "Approve"]
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
